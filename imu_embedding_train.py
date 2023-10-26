bb794d05a1b8e0f8e27df56008af7d930b0f3b5d
Ranak Chowhury <Ritvik Kapila>
2023-10-26 14:45:09 -0700

    minor changes

--- imu_embedding_train.py
+++ imu_embedding_train.py
@@ -23,18 +23,18 @@ def get_embeddings(extrapolated_imu_data, device, out_file):
     final_embedding = np.array([[]])
     # change for different dataset
     for i in range(0, 5):
-        print(i*9, i*9+6)
+        # print(i*9, i*9+6)
         for j in range(0, num_batches):
 
             start_batch = j * batch_size
             end_batch = min(N, (j+1)*batch_size)
-            print(start_batch, end_batch)
+            # print(start_batch, end_batch)
             # change for different dataset
             time_series = extrapolated_imu_data[start_batch:end_batch,:,i*9:i*9+6].astype(np.float32)
             # time_series = np.expand_dims(np.transpose(time_series), axis=0)
             # change for different dataset
             time_series = np.moveaxis(time_series, 1, 2)
-            print('time_series shape: ', time_series.shape)
+            # print('time_series shape: ', time_series.shape)
             # Load data
             inputs = {
                 ModalityType.IMU: torch.from_numpy(time_series).to(device),
@@ -43,7 +43,7 @@ def get_embeddings(extrapolated_imu_data, device, out_file):
                 embeddings = model(inputs)
 
             imu_embedding = (embeddings[ModalityType.IMU].to("cpu").numpy())
-            print('imu_embedding shape: ', imu_embedding.shape)
+            # print('imu_embedding shape: ', imu_embedding.shape)
             # f=open(out_file,'a')
             # np.savetxt(out_file, imu_embedding, fmt='%f')
             if i == 0 and j == 0:
@@ -51,8 +51,8 @@ def get_embeddings(extrapolated_imu_data, device, out_file):
             else:
                 final_embedding = np.concatenate((final_embedding, imu_embedding))
 
-            print('final_embedding shape: ', final_embedding.shape)
-        
+    print('final_embedding shape: ', final_embedding.shape)
+    
     np.save(out_file, final_embedding)
 
 def extrapolate_timeseries(imu_data_path, out_file):
